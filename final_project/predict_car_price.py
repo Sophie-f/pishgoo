a14e04588146a3f0f521031ceb32f450dbdfb41b
fresh <fereshtehghasempour@gmail.com>
2019-01-02 18:24:40 +0330

    add files

--- final_project/predict_car_price.py
+++ final_project/predict_car_price.py
@@ -6,23 +6,25 @@ from sklearn import tree
 
 
 def make_table_in_data_base():
-    cnx = mysql.connector.connect(user = '', password ='', host='127.0.0.1', database = 'car' )
+    cnx = mysql.connector.connect(user='admin', password='123456', host='127.0.0.1', database='car')
     cursor = cnx.cursor()
-    query = 'CREATE TABLE all_cars(name varchar(50),year int, milage int, price bigint, id int ); ALTER TABLE all_cars ADD CONSTRAINT uc_all_cars UNIQUE(id);'
-    cursor.execute(query, multi = True)
+    query = 'CREATE TABLE all_cars(name varchar(50),year int, milage int, price bigint, id int ); ALTER TABLE all_cars'\
+            ' ADD CONSTRAINT uc_all_cars UNIQUE(id);'
+    cursor.execute(query, multi=True)
     cnx.close()
+
 
 def choose_car_name():
     site_request = requests.get('https://bama.ir')
-    soup = BeautifulSoup(site_request.text,'html.parser')
-    cars_list = soup.find_all('span', attrs = {'class': 'home-brand-model-title nav-sub-brand-name-show-text'})
+    soup = BeautifulSoup(site_request.text, 'html.parser')
+    cars_list = soup.find_all('span', attrs={'class': 'home-brand-model-title nav-sub-brand-name-show-text'})
     full_name = ''
     flag = True
     while flag:
         name = input('Enter the brand (in persion): ')    
         for car in cars_list:
             if name == car.text.strip():
-                full_name += (name +',')
+                full_name += (name + ',')
                 opt_car = car
                 flag = False
                 break
@@ -31,7 +33,7 @@ def choose_car_name():
     tag = opt_car.parent['href']
 
     models_list = []
-    persion_model_list =[]
+    persion_model_list = []
     for car in cars_list:
         link = re.findall(r'({}.*)'.format(tag), car.parent['href'])
         if link != []:
@@ -56,20 +58,22 @@ def choose_car_name():
     if versions_list != []:    
         print('Enter the version index')       
         for j in range(0, len(versions_list)):
-            print(j+1,re.findall(r'{}/(.*)'.format(opt_model), versions_list[j])[0])
+            print(j+1, re.findall(r'{}/(.*)'.format(opt_model), versions_list[j])[0])
         opt_version = versions_list[int(input())-1]
-        full_name += (','+ soup.find('a' , attrs = {'href': opt_version } ).contents[1].contents[0].strip()) 
+        full_name += (',' + soup.find('a', attrs={'href': opt_version}).contents[1].contents[0].strip())
     else:
         opt_version = opt_model      
-    print('you piced out:' ,full_name)
-    return 'https://bama.ir'+opt_version , full_name
+    print('you piced out:', full_name)
+    return 'https://bama.ir' + opt_version, full_name
+
 
 def search_car_data(link, full_name):
     def insert_data_to_database(full_name, year, milage, price, car_id):
         flag = False
-        cnx = mysql.connector.connect(user = '', password ='', host='127.0.0.1', database = 'car' )
+        cnx = mysql.connector.connect(user='admin', password='123456', host='127.0.0.1', database='car')
         cursor = cnx.cursor()
-        query = 'INSERT INTO all_cars value(\'{}\',{},{},{},{})'.format(full_name, int(year) , int(milage) , int(price), int(car_id[0]))
+        query = 'INSERT INTO all_cars value(\'{}\',{},{},{},{})'.format(full_name, int(year), int(milage), int(price),
+                                                                        int(car_id[0]))
         try:
             cursor.execute(query)
             flag = True
@@ -84,17 +88,17 @@ def search_car_data(link, full_name):
     page_index = 1
     number_of_car = 500
     while count < number_of_car:
-        request = requests.get(link, allow_redirects = False)
+        request = requests.get(link, allow_redirects=False)
         if request.status_code != 200: 
             break
         soup = BeautifulSoup(request.text, 'html.parser')
-        soup_list = soup.find_all('li', attrs = {'itemtype': "http://schema.org/Car"})
+        soup_list = soup.find_all('li', attrs={'itemtype': "http://schema.org/Car"})
         for soup in soup_list:
             new_milage = ''
-            milage = soup.find('span', attrs = {'class' :"yellow-background"})
-            price = soup.find_all(itemprop = 'price' )    
-            #we do not consider the car with double price(installment and prepayment or the car without milage) 
-            if len(price) != 1 or  milage is None: 
+            milage = soup.find('span', attrs={'class': "yellow-background"})
+            price = soup.find_all(itemprop='price')
+            # we do not consider the car with double price(installment and prepayment or the car without milage)
+            if len(price) != 1 or milage is None:
                 continue
             # we do not consider the car without definit price( like تماس بگیرید، در توضیحات)
             price = price[0]['content']
@@ -107,10 +111,10 @@ def search_car_data(link, full_name):
                     c = '0'
                 new_milage += c
 
-            id_soup = soup.find('a', attrs = {'class': 'cartitle cartitle-desktop'})
+            id_soup = soup.find('a', attrs={'class': 'cartitle cartitle-desktop'})
             car_id = re.findall(r'car/details-.*-(.*)/', id_soup['href'])
             
-            details = soup.find('h2',attrs = {'itemprop': 'name'}).text.split('،')
+            details = soup.find('h2', attrs={'itemprop': 'name'}).text.split('،')
             year = details[0].strip()
 
             if insert_data_to_database(full_name, year, new_milage, price, car_id):
@@ -119,16 +123,17 @@ def search_car_data(link, full_name):
         page_index += 1            
         link = prime_link+'?page='+str(page_index)
 
+
 def read_analyse_data(name):
-    cnx = mysql.connector.connect(user = '', password ='', host='127.0.0.1', database = 'car' )
+    cnx = mysql.connector.connect(user='admin', password='123456', host='127.0.0.1', database='car')
     cursor = cnx.cursor()
     x = []
     y = []
     query = 'SELECT year, milage, price FROM all_cars where name = \'{}\''.format(name)
     cursor.execute(query)
     flag = False
-    for year, milage, price  in cursor:
-        x.append([year,milage])
+    for year, milage, price in cursor:
+        x.append([year, milage])
         y.append(price)
         flag = True
     if flag:
@@ -136,8 +141,8 @@ def read_analyse_data(name):
         clf = clf.fit(x, y)
         year_data = input('Enetr the year, examples: 1397 or 2018 : ')
         milage_data = input('Enter the milage (kilometer): ')
-        answer = clf.predict([[year_data , milage_data]])
-        print('The price is {:,} toman'.format(answer[0],grouping = True))
+        answer = clf.predict([[year_data, milage_data]])
+        print('The price is {:,} toman'.format(answer[0], grouping=True))
     else:
         print('Your car does not exist in the database.')
     cnx.close()  
---
+++ search_car_price/search_car_price2.py
@@ -0,0 +1,107 @@
+import re
+from bs4 import BeautifulSoup
+import mysql.connector
+import requests
+site_request = requests.get('https://bama.ir')
+soup = BeautifulSoup(site_request.text,'html.parser')
+cars_list = soup.find_all('span', attrs = {'class': 'home-brand-model-title nav-sub-brand-name-show-text'})
+
+
+
+def search_car(link, max_num):
+    prime_link = link
+    count = 0
+    page_index = 1
+    while count <= max_num :
+        request = requests.get('https://bama.ir' + link, allow_redirects = False)
+        if request.status_code != 200: 
+            break
+        soup = BeautifulSoup(request.text, 'html.parser')
+        soup_list = soup.find_all('li', attrs = {'itemtype': "http://schema.org/Car"})
+        for soup in soup_list:
+            new_milage = ''
+            milage = soup.find('span', attrs = {'class' :"yellow-background"})
+            price = soup.find_all(itemprop = 'price' )
+
+             # we do not consider the car with double price(installment and prepayment and the car without milage) 
+            if len(price) != 1 or  milage is None: 
+                continue
+            # we do not consider the car without definit price( like تماس بگیرید، در توضیحات)
+            price = price[0]['content']
+            if price == '0':
+                continue
+                
+
+            count += 1 
+            if count > max_num:
+                break   
+
+            details = soup.find('h2',attrs = {'itemprop': 'name'}).text.split('،')
+            for i in range(len(details)):
+                details[i] = details[i].strip()
+            year = details[0]
+
+            milage_list = milage.next_sibling.strip().split(',')
+            for c in milage_list:
+                if c == 'صفر':
+                    c = '0'
+                new_milage += c
+            cnx = mysql.connector.connect(user = 'admin', password ='123456', host='127.0.0.1', database = 'car' )
+            cursor = cnx.cursor()
+            query = 'INSERT INTO car_price value({},{},{})'.format(int(year), int(new_milage), int(price))
+            cursor.execute(query)
+            cnx.commit()
+            cnx.close()  
+        page_index += 1            
+        link = prime_link+'?page='+str(page_index)
+
+
+
+
+max_num = int(input('Enter maximum number of car: ')) 
+flag = True
+while flag:
+    name = input('Enter the brand (in persian): ')    
+    for car in cars_list:
+        if name == car.text.strip():
+            opt_car = car
+            flag = False
+            break
+    if flag:        
+        print('This brand does not exist.')
+tag = opt_car.parent['href']
+
+models_list = []
+for car in cars_list:
+    link = re.findall(r'({}.*)'.format(tag), car.parent['href'])
+    if link != []:
+        if link[0] not in models_list:
+            models_list.append(link[0])
+print("Enter the number of model")
+print(1, 'all models')         
+for i in range(1, len(models_list)):
+    model = re.findall(r'{}/(.*)'.format(tag) ,models_list[i])
+    print(i+1, model[0])
+m_index = int(input())
+opt_model = models_list[m_index-1]
+
+if m_index == 1:
+   search_car(opt_model, max_num)
+else:       
+    versions_list = []
+    links = re.findall(r'\"({}/.*?)\"'.format(opt_model), site_request.text)
+    for link in links:
+        if link not in versions_list:
+            versions_list.append(link)
+    print('Enter the version index')   
+    print(1,'All versions')    
+
+    
+    for j in range(0, len(versions_list)):
+        print(j+2,re.findall(r'{}/(.*)'.format(opt_model), versions_list[j])[0])
+    v_index = int(input()) 
+    if v_index == 1:
+        search_car(opt_model, max_num) 
+    else:        
+        search_car(versions_list[v_index-2], max_num)
+
